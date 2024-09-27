3bf9a719eb3387c4ec396a5ab3170e79526585c4
Alisa250606 <kn23-a.daieva@nubip.edu.ua>
2024-09-27 11:28:03 +0300

    Update content in submodule

--- tasks/task2.js
+++ tasks/task2.js
@@ -2,10 +2,21 @@
 //  при цьому пропускаючи всі цифри.
 
 function reverseWithoutNumbers(str) {
-  // Ваш код тут
+  let reversed = ''; 
+
+  
+  for (let i = str.length - 1; i >= 0; i--) {
+    
+    if (isNaN(str[i])) {
+      reversed += str[i];
+    }
+  }
+
+  return reversed; 
 }
 
+// Приклади використання:
 console.log(reverseWithoutNumbers("hello123world456")); // Виведе: "dlrowolleh"
 console.log(reverseWithoutNumbers("abc123xyz"));       // Виведе: "zyxabc"
 
-module.exports = reverseWithoutNumbers;
+module.exports = reverseWithoutNumbers; // Експорт функції
--- tasks/task5.js
+++ tasks/task5.js
@@ -1,13 +1,21 @@
 // Задача: Напишіть функцію countOccurrences, яка приймає рядок і символ,
 //  і повертає кількість входжень цього символу в рядку.
 
-
 function countOccurrences(str, char) {
-  //Ваш код
+  let count = 0; 
+
+  // Проходимо через рядок
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+
+  return count; // Повертаємо кількість входжень
 }
 
 // Приклади використання:
 console.log(countOccurrences("hello world", 'o')); // Виведе: 2
 console.log(countOccurrences("banana", 'a'));       // Виведе: 3
 
-module.exports = countOccurrences;
+module.exports = countOccurrences; // Експорт функції
